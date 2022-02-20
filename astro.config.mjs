24b5519c7ee4e0dce1e6ec07ac5ba583e4aa51ed
ooliver1 <oliverwilkes2006@icloud.com>
2022-02-20 10:21:55 +0000

    feat: proper template for twcss finally

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: "https://tooty.xyz", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    site: "https://u.rl", // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true, // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
